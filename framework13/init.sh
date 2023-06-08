b9a92fea6b4afa74cb05a551cdca4db09f7aa03b
Dan Jordan <dan.jordan@highspeedtraining.co.uk>
2023-06-08 22:14:56 +0100

    try nvme

--- framework13/init.sh
+++ framework13/init.sh
@@ -1,18 +1,18 @@
-parted /dev/sda -- mklabel gpt
-parted /dev/sda -- mkpart primary 512MB -8GB
-parted /dev/sda -- mkpart primary linux-swap -8GB 100%
+parted /dev/nvme0n1 -- mklabel gpt
+parted /dev/nvme0n1 -- mkpart primary 512MB -8GB
+parted /dev/nvme0n1 -- mkpart primary linux-swap -8GB 100%
 
-parted /dev/sda -- mkpart ESP fat32 1MB 512MB
-parted /dev/sda -- set 3 esp on
+parted /dev/nvme0n1 -- mkpart ESP fat32 1MB 512MB
+parted /dev/nvme0n1 -- set 3 esp on
 
-mkfs.ext4 -L nixos /dev/sda1
-mkswap -L swap /dev/sda2
-mkfs.fat -F 32 -n boot /dev/sda3
+mkfs.ext4 -L nixos /dev/nvme0n11
+mkswap -L swap /dev/nvme0n12
+mkfs.fat -F 32 -n boot /dev/nvme0n13
 
 mount /dev/disk/by-label/nixos /mnt
 mkdir -p /mnt/boot
 mount /dev/disk/by-label/boot /mnt/boot
-swapon /dev/sda2
+swapon /dev/nvme0n12
 
 nixos-generate-config --root /mnt
 #wget https://raw.githubusercontent.com/deej81/setup/main/framework13/configuration.nix -P /mnt/etc/nixos
