762054aa9da17f7a3083437159285f0d28efe7c4
Dan Jordan <dan.jordan@highspeedtraining.co.uk>
2023-06-08 22:09:26 +0100

    baseline

---
+++ framework13/init.sh
@@ -0,0 +1,20 @@
+parted /dev/sda -- mklabel gpt
+parted /dev/sda -- mkpart primary 512MB -8GB
+parted /dev/sda -- mkpart primary linux-swap -8GB 100%
+
+parted /dev/sda -- mkpart ESP fat32 1MB 512MB
+parted /dev/sda -- set 3 esp on
+
+mkfs.ext4 -L nixos /dev/sda1
+mkswap -L swap /dev/sda2
+mkfs.fat -F 32 -n boot /dev/sda3
+
+mount /dev/disk/by-label/nixos /mnt
+mkdir -p /mnt/boot
+mount /dev/disk/by-label/boot /mnt/boot
+swapon /dev/sda2
+
+nixos-generate-config --root /mnt
+#wget https://raw.githubusercontent.com/deej81/setup/main/framework13/configuration.nix -P /mnt/etc/nixos
+
+nixos-install
