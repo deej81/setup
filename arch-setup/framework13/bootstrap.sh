945376c8deb097dc1bb0332d7f2689efdd35df04
Dan Jordan <dan.jordan@highspeedtraining.co.uk>
2023-06-26 16:48:37 +0100

    update mirrors

--- arch-setup/framework13/bootstrap.sh
+++ arch-setup/framework13/bootstrap.sh
@@ -18,6 +18,9 @@ parted /dev/nvme0n1 --script -- mklabel gpt \
 # parted /dev/nvme0n1 -- mkpart ESP fat32 1MB 512MB
 # parted /dev/nvme0n1 -- set 3 esp on
 
+# generate mirror list for united kingdom
+curl -s "https://archlinux.org/mirrorlist/?country=GB&protocol=https&ip_version=4" | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n 5 - > /etc/pacman.d/mirrorlist
+
 mkfs.ext4 -L arch /dev/nvme0n1p1
 mkswap -L swap /dev/nvme0n1p2
 mkfs.fat -F 32 -n boot /dev/nvme0n1p3
