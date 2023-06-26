02fea6e21c7de773b67856aa2ffd26209338abad
Dan Jordan <dan.jordan@highspeedtraining.co.uk>
2023-06-26 16:42:00 +0100

    script parted

--- arch-setup/framework13/bootstrap.sh
+++ arch-setup/framework13/bootstrap.sh
@@ -4,12 +4,19 @@ loadkeys uk
 
 timedatectl set-timezone Europe/London
 
-parted /dev/nvme0n1 -- mklabel gpt
-parted /dev/nvme0n1 -- mkpart primary 512MB -8GB
-parted /dev/nvme0n1 -- mkpart primary linux-swap -8GB 100%
+# run parted unattended
+parted /dev/nvme0n1 --script -- mklabel gpt \
+    mkpart primary 512MB -8GB \
+    mkpart primary linux-swap -8GB 100% \
+    mkpart ESP fat32 1MB 512MB \
+    set 3 esp on
 
-parted /dev/nvme0n1 -- mkpart ESP fat32 1MB 512MB
-parted /dev/nvme0n1 -- set 3 esp on
+# parted /dev/nvme0n1 -- mklabel gpt
+# parted /dev/nvme0n1 -- mkpart primary 512MB -8GB
+# parted /dev/nvme0n1 -- mkpart primary linux-swap -8GB 100%
+
+# parted /dev/nvme0n1 -- mkpart ESP fat32 1MB 512MB
+# parted /dev/nvme0n1 -- set 3 esp on
 
 mkfs.ext4 -L arch /dev/nvme0n1p1
 mkswap -L swap /dev/nvme0n1p2
@@ -20,13 +27,10 @@ mkdir -p /mnt/boot
 mount /dev/nvme0n1p3 /mnt/boot
 swapon /dev/nvme0n1p2
 
-# select uk mirror
-curl -s "https://www.archlinux.org/mirrorlist/?country=GB&protocol=https&ip_version=4&use_mirror_status=on" | sed -e 's/^#Server/Server/' -e '/^#/d' > /etc/pacman.d/mirrorlist
-
 pacstrap -K /mnt base base-devel linux linux-firmware sof-firmware neovim git grub efibootmgr networkmanager wget
 
 genfstab -U /mnt >> /mnt/etc/fstab
 
 cp init.sh /mnt
-arch-chroot /mnt ./init.sh
+arch-chroot /mnt sh ./init.sh
 
