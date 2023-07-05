eafc998e130bfcfd5f4588969717e621a9a8801a
Dan Jordan <dan.jordan@highspeedtraining.co.uk>
2023-07-05 13:38:34 +0100

    remove quotes

--- arch-setup/vm/bootstrap.sh
+++ arch-setup/vm/bootstrap.sh
@@ -30,5 +30,5 @@ pacstrap -K /mnt base base-devel linux linux-firmware sof-firmware neovim git gr
 genfstab -U /mnt >> /mnt/etc/fstab
 
 cp init.sh /mnt
-arch-chroot /mnt sh "./init.sh $USERNAME $HOSTNAME"
+arch-chroot /mnt sh ./init.sh $USERNAME $HOSTNAME
 
