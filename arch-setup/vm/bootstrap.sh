c38d00995257237d59ccd85ac55791076f5f5e2d
Dan Jordan <dan.jordan@highspeedtraining.co.uk>
2023-07-05 10:32:56 +0100

    try fix vm

--- arch-setup/vm/bootstrap.sh
+++ arch-setup/vm/bootstrap.sh
@@ -8,10 +8,10 @@ timedatectl set-timezone Europe/London
 
 #partition disk vda for vm - non UEFI
 parted /dev/vda --script -- mklabel gpt \
-    mkpart primary ext3 1MiB 100MiB \
-    set 1 boot on \
-    mkpart primary linux-swap 100MiB 4GiB \
-    mkpart primary ext4 4GiB 100%
+    mkpart primary ext4 1MiB 500MiB \
+    set 1 bios_grub on \
+    mkpart primary linux-swap 500MiB 5GiB \
+    mkpart primary ext4 5GiB 100%
 
 mkfs.ext4 -L arch /dev/vda2
 mkswap -L swap /dev/vda3
@@ -19,9 +19,6 @@ mkswap -L swap /dev/vda3
 
 mount /dev/vda2 /mnt
 swapon /dev/vda3
-
-mkdir /mnt/boot
-mount /dev/vda1 /mnt/boot
 
 # generate mirror list for united kingdom
 curl -s "https://archlinux.org/mirrorlist/?country=GB&protocol=https&ip_version=4" | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n 5 - > /etc/pacman.d/mirrorlist
